a1184a24bd1c960953ebf3e80915ca4b644972a2
mmmihhaeel <mikael11032005@gmail.com>
2023-08-16 23:04:05 +0300

    updated image

--- configuration/iaac/azure/kubernetes/variables.tf
+++ configuration/iaac/azure/kubernetes/variables.tf
@@ -1,11 +1,11 @@
 variable "client_id" {
-  default = "46cc859d-f5d2-489c-825b-6273f056f08b"
+
 }
 variable "client_secret" {
-  default = "yGp8Q~fmKUCNWpzUktz0trLh5bLUlIhbX35VRcYW"
+
 }
 variable "ssh_public_key" {
-  default = "~/.ssh/azure_rsa.pub"
+
 }
 variable "environment" {
   default = "dev"
