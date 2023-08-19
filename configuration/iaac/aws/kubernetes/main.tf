dfeecd4d0771b4fe80ee6f833bfc476eaec2f724
mmmihhaeel <mikael11032005@gmail.com>
2023-08-19 12:47:08 +0300

    fix

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,8 +1,6 @@
 # aws --version
 # aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
 # Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
-# terraform-backend-state-in28minutes-123
-# AKIA4AHVNOD7OOO6T4KI
 
 terraform {
   backend "s3" {
@@ -32,23 +30,124 @@ provider "kubernetes" {
 }
 
 module "top-backend-starter-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "top-backend-starter-cluster"
-  cluster_version = "1.14"
-  # subnets         = var.subnets #CHANGE
-  subnets = ["subnet-0cd8305def97ceef0", "subnet-0db997e3bc22720bc", "subnet-0b387645c7906d6c4"]
-  vpc_id  = aws_default_vpc.default.id
-
-  #vpc_id         = "vpc-1234556abcdef"
-
-  node_groups = [
-    {
-      instance_type    = "t2.micro"
-      max_capacity     = 5
-      desired_capacity = 3
-      min_capacity     = 3
+  version                        = "~> 19.0"
+  source                         = "terraform-aws-modules/eks/aws"
+  cluster_name                   = "top-backend-starter-cluster"
+  cluster_version                = "1.27"
+  cluster_endpoint_public_access = true
+  subnet_ids                     = var.subnets
+  vpc_id                         = aws_default_vpc.default.id
+  cluster_addons = {
+    coredns = {
+      most_recent = true
     }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    instance_type                          = "m6i.large"
+    update_launch_template_default_version = true
+    iam_role_additional_policies = {
+      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    }
+  }
+
+  self_managed_node_groups = {
+    one = {
+      name         = "mixed-1"
+      max_size     = 5
+      desired_size = 2
+
+      use_mixed_instances_policy = true
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity                  = 0
+          on_demand_percentage_above_base_capacity = 10
+          spot_allocation_strategy                 = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "m5.large"
+            weighted_capacity = "1"
+          },
+          {
+            instance_type     = "m6i.large"
+            weighted_capacity = "2"
+          },
+        ]
+      }
+    }
+  }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Fargate Profile(s)
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "default"
+        }
+      ]
+    }
+  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::66666666666:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
   ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user1"
+      username = "user1"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user2"
+      username = "user2"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_accounts = [
+    "777777777777",
+    "888888888888",
+  ]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
 
 data "aws_eks_cluster" "cluster" {
