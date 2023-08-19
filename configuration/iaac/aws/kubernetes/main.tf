227ac9d64110cb0bbe07338102d3349378a5ff7c
mmmihhaeel <mikael11032005@gmail.com>
2023-08-19 17:02:04 +0300

    fix

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,64 +1,159 @@
 # aws --version
-# aws eks --region us-east-1 update-kubeconfig --name top-backend-starter-cluster
+# aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
 # Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
 # terraform-backend-state-in28minutes-123
 # AKIA4AHVNOD7OOO6T4KI
+
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
   backend "s3" {
-    bucket = "mybucket"       # Will be overridden from build
-    key    = "path/to/my/key" # Will be overridden from build
-    region = "us-east-1"
+    bucket         = "mybucket"       # Will be overridden from build
+    key            = "path/to/my/key" # Will be overridden from build
+    region         = "us-east-1"
+    dynamodb_table = "dev_application_locks"
+    encrypt        = true
   }
 }
 
 resource "aws_default_vpc" "default" {
-
+  tags = {
+    Name = "Default VPC"
+  }
 }
-
-# data "aws_subnet_ids" "subnets" {
-#   vpc_id = aws_default_vpc.default.id
-# }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  version                = "~> 2.12"
 }
 
 module "top-backend-starter-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "top-backend-starter-cluster"
-  cluster_version = "1.14"
-  subnet_ids      = ["subnet-04401d58dd83dbaef", "subnet-015186c2978f8030a", "subnet-0b2907424664555b7"] #CHANGE
-  #subnets = data.aws_subnet_ids.subnets.ids
-  vpc_id = aws_default_vpc.default.id
+  version                        = "~> 19.0"
+  source                         = "terraform-aws-modules/eks/aws"
+  cluster_name                   = "top-backend-starter-cluster"
+  cluster_version                = "1.27"
+  cluster_endpoint_public_access = true
+  subnet_ids                     = var.subnets
+  vpc_id                         = aws_default_vpc.default.id
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
-  #vpc_id         = "vpc-1234556abcdef"
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    instance_type                          = "m6i.large"
+    update_launch_template_default_version = true
+    iam_role_additional_policies = {
+      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    }
+  }
 
-  #   node_groups = [
-  #     {
-  #       instance_type    = "t2.micro"
-  #       max_capacity     = 5
-  #       desired_capacity = 3
-  #       min_capacity     = 3
-  #     }
-  #   ]
+  self_managed_node_groups = {
+    one = {
+      name         = "mixed-1"
+      max_size     = 5
+      desired_size = 2
+
+      use_mixed_instances_policy = true
+      mixed_instances_policy = {
+        instances_distribution = {
+          on_demand_base_capacity                  = 0
+          on_demand_percentage_above_base_capacity = 10
+          spot_allocation_strategy                 = "capacity-optimized"
+        }
+
+        override = [
+          {
+            instance_type     = "m5.large"
+            weighted_capacity = "1"
+          },
+          {
+            instance_type     = "m6i.large"
+            weighted_capacity = "2"
+          },
+        ]
+      }
+    }
+  }
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Fargate Profile(s)
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "default"
+        }
+      ]
+    }
+  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  aws_auth_roles = [
+    {
+      rolearn  = "arn:aws:iam::66666666666:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user1"
+      username = "user1"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user2"
+      username = "user2"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  aws_auth_accounts = [
+    "777777777777",
+    "888888888888",
+  ]
+
+  tags = {
+    Environment = "prod"
+    Terraform   = "true"
+  }
 }
 
-# data "aws_eks_cluster" "cluster" {
-#   name = module.top-backend-starter-cluster.cluster_id
-# }
+data "aws_eks_cluster" "cluster" {
+  name = module.top-backend-starter-cluster.cluster_id
+}
 
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.top-backend-starter-cluster.cluster_id
-# }
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.top-backend-starter-cluster.cluster_id
+}
 
 
 # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
@@ -80,7 +175,6 @@ resource "kubernetes_cluster_role_binding" "example" {
   }
 }
 
-# Needed to set the default region
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
