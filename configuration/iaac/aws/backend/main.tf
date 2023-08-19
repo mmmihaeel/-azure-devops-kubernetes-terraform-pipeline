b29fe6697eb6fa7e559f12b0a154692ecec79c1e
mmmihhaeel <mikael11032005@gmail.com>
2023-08-19 18:50:02 +0300

    fixed backend

--- configuration/iaac/aws/backend/main.tf
+++ configuration/iaac/aws/backend/main.tf
@@ -39,7 +39,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
 }
 
 resource "aws_dynamodb_table" "enterprise_backend_lock" {
-  name         = "dev_application_locks"
+  name         = "prod_application_locks"
   billing_mode = "PAY_PER_REQUEST"
 
   hash_key = "LockID"
